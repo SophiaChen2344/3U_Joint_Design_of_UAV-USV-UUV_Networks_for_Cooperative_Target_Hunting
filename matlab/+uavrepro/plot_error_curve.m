2335791b0e7b00410978445adb4ef9d1c6fa1494
SophiaChen2344 <sophiadchen@icloud.com>
2026-04-25 03:06:13 +0800

    Create plot_error_curve.m

---
+++ matlab/+uavrepro/plot_error_curve.m
@@ -0,0 +1,27 @@
+function plot_error_curve(time, error, controller, axis_name, params)
+% 绘制误差曲线（Fig.11-16）
+figure('Color','w','Position',[300,100,800,500]);
+hold on; grid on;
+
+% UAV颜色匹配论文
+colors = ['b','r','y','m','g','k'];
+labels = {'UAV1','UAV2','UAV3','UAV4','UAV5'};
+
+% 绘制每架UAV的误差
+for i = 2:params.uav_num % 跳过leader(UAV0)
+    plot(time, error(i,:), 'Color',colors(i),'LineWidth',1.5,'DisplayName',labels{i-1});
+end
+
+% 图表配置
+xlabel('t(s)','FontSize',12);
+if axis_name == 'x'
+    ylabel(['e_{i',axis_name,'}(m)'],'FontSize',12);
+    title(['Evolution of formation error e_{i',axis_name,'} with the ',controller,' method'],'FontSize',12);
+else
+    ylabel(['e_{i',axis_name,'}(m)'],'FontSize',12);
+    title(['Evolution of formation error e_{i',axis_name,'} with the ',controller,' method'],'FontSize',12);
+end
+ylim([-20, 30]); % 匹配论文刻度
+xticks(0:20:80);
+legend('Location','best');
+end
